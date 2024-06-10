c52b44af8b267127367d0a80115ed37a57b133e2
Damisola Morawo <morawodamisolataiwo77@gmail.com>
2024-06-10 08:36:27 +0100

    Add built APK

---
+++ .gitignore.dart
@@ -0,0 +1,18 @@
+# Flutter/Dart/Pub related
+    .dart_tool/
+    .flutter-plugins
+    .flutter-plugins-dependencies
+    .packages
+    .pub-cache/
+    .pub/
+build/
+ios/Flutter/.last_build_id
+
+# Android related
+    .gradle/
+    .idea/
+    .android/
+local.properties
+
+# VS Code related
+.vscode/
